511bf6e247fd621a29f8bc00e8d85170e206a8a3
niranjanmesta <115413373+niranjanmesta@users.noreply.github.com>
2023-09-27 08:24:13 +0530

    Update 02_dataTypes.js

---
+++ basic_js/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log("Welcome to my javascript leaning")
+
+let name= "Niranjan"//string data type
+let age = 21 //number data type
+let isLoggedLn = false//boolean data type
+let state = null;
+let city;
+
+
+//Number => 2 to power 53 
+// bigint  
+//string =>""
+//boolean => true/false
+//null => stamdalone value
+//undifined =>
+//symbol =>unique
+
+//object 
+console.log(typeof "Niranjan");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age,isLoggedLn,city,state)
