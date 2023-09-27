8e23fe03299b50972765be7719b1c65d6c095791
niranjanmesta <115413373+niranjanmesta@users.noreply.github.com>
2023-09-27 08:08:37 +0530

    Add files via upload

---
+++ basic_js/01_variables.js
@@ -0,0 +1,17 @@
+const Account_Id = 2023
+let Account_Email ="niranjanmesta1234@gmail.com";
+var password = "Niranjan";
+accountcity = "surat"
+let account_State;
+//account_id = 2024  //const not allowed 
+
+console.log(Account_Id)
+/*
+prefer not to use var
+because of issue in block space and functional scope
+*/
+password ="sachin"
+accountcity="vapi"
+Account_Email="niaranjan14@gmail.com"
+
+console.table([Account_Id,Account_Email,password,accountcity,account_State])
